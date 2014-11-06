4a09b7c584e2e125bc1f0d1ee33ca347034094ee
zednis <lokipatera@gmail.com>
2014-11-06 14:24:27 -0700

    consolidated example prefixes

--- examples/prov-primer-example.py
+++ examples/prov-primer-example.py
@@ -1,7 +1,7 @@
 __author__ = 'szednik'
 
 """
-This example was taken from the running example in http://www.w3.org/TR/prov-primer
+This example was taken from http://www.w3.org/TR/prov-primer
 """
 
 from kleio import prov
@@ -15,31 +15,28 @@ prov.graph.bind("dct", DCT)
 FOAF = Namespace("http://xmlns.com/foaf/0.1/")
 prov.graph.bind("foaf", FOAF)
 
-prov.bind_ns("exc", "http://www.example.org#")
-prov.bind_ns("exg", "http://www.example.org#")
-prov.bind_ns("exn", "http://www.example.org#")
-prov.bind_ns("exb", "http://www.example.org#")
+prov.bind_ns("ex", "http://www.example.org#")
 
 # Entities
 
-article = prov.Entity("exn:article")
+article = prov.Entity("ex:article")
 article.add(DCT.title, rdflib.Literal("Crime rises in cities"))
 
-dataset1 = prov.Entity("exg:dataset1")
+dataset1 = prov.Entity("ex:dataset1")
 
-regionList = prov.Entity("exc:regionList")
+regionList = prov.Entity("ex:regionList")
 
-composition1 = prov.Entity("exc:composition1")
+composition1 = prov.Entity("ex:composition1")
 
-chart1 = prov.Entity("exc:chart1")
+chart1 = prov.Entity("ex:chart1")
 
 # Activities
 
-compile1 = prov.Activity("exc:compile1")
+compile1 = prov.Activity("ex:compile1")
 
-compose1 = prov.Activity("exc:compose1")
+compose1 = prov.Activity("ex:compose1")
 
-illustrate1 = prov.Activity("exc:illustrate1")
+illustrate1 = prov.Activity("ex:illustrate1")
 
 # Usage and Generation
 
@@ -54,11 +51,11 @@ chart1.set_was_generated_by(illustrate1)
 
 # Agents and Responsibility
 
-derek = prov.Person("exc:derek")
+derek = prov.Person("ex:derek")
 derek.add(FOAF.givenName, rdflib.Literal("Derek"))
 derek.add(FOAF.mbox, rdflib.URIRef("mailto:derek@example.org"))
 
-chartgen = prov.Organization("exc:chartgen")
+chartgen = prov.Organization("ex:chartgen")
 chartgen.add(FOAF.name, rdflib.Literal("Chart Generators Inc"))
 
 compose1.set_was_associated_with(derek)
@@ -69,10 +66,10 @@ derek.set_acted_on_behalf_of(chartgen)
 
 # Roles
 
-dataToCompose = prov.Role("exc:dataToCompose")
-regionsToAggregateBy = prov.Role("exc:regionsToAggregateBy")
-composedData = prov.Role("exc:composedData")
-analyst = prov.Role("exc:analyst")
+dataToCompose = prov.Role("ex:dataToCompose")
+regionsToAggregateBy = prov.Role("ex:regionsToAggregateBy")
+composedData = prov.Role("ex:composedData")
+analyst = prov.Role("ex:analyst")
 
 compose1.usage(entity=dataset1, role=dataToCompose)
 compose1.usage(entity=regionList, role=regionsToAggregateBy)
@@ -81,19 +78,19 @@ composition1.generation(activity=compose1, role=composedData)
 
 # Revision and Derivation
 
-dataset2 = prov.Entity("exg:dataSet2")
+dataset2 = prov.Entity("ex:dataset2")
 dataset2.set_was_revision_of(dataset1)
 
-chart2 = prov.Entity("exc:chart2")
+chart2 = prov.Entity("ex:chart2")
 chart2.set_was_derived_from(dataset2)
 chart2.set_was_revision_of(chart1)
 
 # Plans
 
-correct1 = prov.Activity("exg:correct1")
-instructions = prov.Plan("exg:instructions")
+correct1 = prov.Activity("ex:correct1")
+instructions = prov.Plan("ex:instructions")
 
-edith = prov.Person("exg:edith")
+edith = prov.Person("ex:edith")
 
 correct1.association(agent=edith, plan=instructions)
 
@@ -115,13 +112,13 @@ correct1.set_ended_at_time(dt4)
 
 # Alternate entities and specialization
 
-quoteInBlogEntry = prov.Entity("exb:quoteInBlogEntry-20130326")
+quoteInBlogEntry = prov.Entity("ex:quoteInBlogEntry-20130326")
 quoteInBlogEntry.set_was_quoted_from(article)
 
-article_v1 = prov.Entity("exn:articleV1")
+article_v1 = prov.Entity("ex:article_v1")
 article_v1.set_specialization_of(article)
 
-article_v2 = prov.Entity("exn:articleV2")
+article_v2 = prov.Entity("ex:article_v2")
 article_v2.set_specialization_of(article)
 
 article_v2.set_alternate_of(article_v1)

cd92fcd74e0dc44b9728da8e384f31e0bc064060
zednis <lokipatera@gmail.com>
2014-11-10 01:33:44 -0700

    updated bundle entity URI to match bundle container URI

--- examples/bundle-example.py
+++ examples/bundle-example.py
@@ -9,7 +9,7 @@ bundle = prov.bundle(id="test:bundle")
 entity = prov.Entity(id="test:entity-in-bundle", bundle=bundle)
 entity.set_label("entity in bundle")
 
-bundle_entity = prov.Bundle(id="test:bundle-entity", bundle=bundle)
+bundle_entity = prov.Bundle(id="test:bundle", bundle=bundle)
 bundle_entity.set_label("bundle entity")
 
 entity_not_in_bundle = prov.Entity(id="test:entity-not-in-bundle")

bec2d076240de9127377a2d6e80f0c84c0453349
zednis <lokipatera@gmail.com>
2014-11-10 01:20:45 -0700

    Updates to bundle example

--- examples/bundle-example.py
+++ examples/bundle-example.py
@@ -1,11 +1,8 @@
 __author__ = 'szednik'
 
 from kleio import prov
-import rdflib
 
-TEST = rdflib.Namespace("http://test.com/ns#")
 prov.bind_ns("test", "http://test.com/ns#")
-prov.ds.bind("test", TEST)
 
 bundle = prov.bundle(id="test:bundle")
 
@@ -19,6 +16,7 @@ entity_not_in_bundle = prov.Entity(id="test:entity-not-in-bundle")
 entity_not_in_bundle.set_label("entity not in bundle")
 
 # Serialize using TriG to see the bundle container as a named graph
+# and PROV assertions not associated with a bundle in the default graph
 print(prov.serialize(format="trig"))
 
 # Serialize using the bundle parameter to see only PROV
